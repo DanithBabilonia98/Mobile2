b3eff9002b262c467da0ca95ddef2a63b07ce096
Danith Babilonia Meza <dbabilonia@lsv-tech.com>
2022-06-06 16:41:17 -0500

    Alerts

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_2/views/listview_2.dart';
+import 'package:flutter_application_2/views/listview_3.dart';
 
 void main() => runApp(const MyApp());
 
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: 'Material App',
-      home: ListView2(),
+      home: ActivityListPage(),
     );
   }
 }
