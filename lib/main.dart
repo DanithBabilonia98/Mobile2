5314aaf7e79c1cc55abd3b9dd99626496ebed658
Danith Babilonia Meza <dbabilonia@lsv-tech.com>
2022-06-09 00:46:20 -0500

    Connection Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,34 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_crashlytics/firebase_crashlytics.dart';
-import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:flutter_application_2/firebase_options.dart';
+import 'package:flutter_application_2/views/listview.dart';
+import 'package:flutter_application_2/views/listview_2.dart';
 import 'package:flutter_application_2/views/listview_3.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   callDatabase();
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      title: 'Material App',
-      home: ActivityListPage(),
-    );
+        debugShowCheckedModeBanner: false,
+        title: 'Material App',
+        home: ListView1());
   }
 }
 
 void callDatabase() {
-  DatabaseReference database = FirebaseDatabase.instance.ref('/Carros/0');
-  database.onValue.listen((event) {
+  DatabaseReference starCountRef =
+      FirebaseDatabase.instance.ref('/Registros/2345600');
+  starCountRef.onValue.listen((event) {
     final data = event.snapshot.value;
     print(data.toString());
   });
